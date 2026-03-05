35cf43acceaca5611b34c4db32076e034d4a57b8
Infinite Lambda <IL@Viets-MacBook-Pro.local>
2026-03-05 22:03:56 +0700

    update zshrc

--- nvim/lua/plugins/language.lua
+++ nvim/lua/plugins/language.lua
@@ -33,6 +33,9 @@ local lsp_servers_config = {
       },
     },
   },
+
+  -- Github Action YAML
+  gh_actions_ls = {},
 }
 
 local linter_config = {
@@ -470,7 +473,7 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'terraform', 'hcl' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'terraform', 'hcl', 'yaml' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
